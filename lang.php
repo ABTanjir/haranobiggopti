ef8ac57cd3bad3151bb437ac2bc11e844761ed51
ABTanjir <ABTanjir@users.noreply.github.com>
2017-10-05 01:48:08 +0600

    language file for further development
    haranobiggopti language file basic structure

---
+++ lang.php
@@ -0,0 +1,36 @@
+<?php
+
+$__lang = array(
+			'en' => array(
+					'home' => 'Home',
+					'all_notice' => 'All Notice',
+					'create_account' => 'Create Account',
+					'login' => 'Login',
+					'dashboard' => 'Dashboard',
+					'logout' => 'Logout',
+					'notification' => 'Notification',
+
+					'lost' => 'Lost',
+					'found' => 'Found',
+					'post_your_notice' => 'Post Your Notice',
+					'wc_heading' => 'Welcome to Haranobiggopti.com'
+
+			),
+			'bn' => array(
+					'home' => 'হোম',
+					'all_notice' => 'সকল বিজ্ঞপ্তি',
+					'create_account' => 'একাউন্ট খুলুন',
+					'login' => 'লগইন',
+					'dashboard' => 'ড্যাশবোর্ড',
+					'logout' => 'লগ আউট',
+					'notification' => 'বার্তা',
+
+					'lost' => 'হারিয়েছি',
+					'found' => 'পেয়েছি',
+					'post_your_notice' => 'আপনার বিজ্ঞপ্তি দিন',
+					'wc_heading' => 'Haranobiggopti.com এ আপনাকে স্বাগতম'
+			)
+		);
+		
+
+?>
